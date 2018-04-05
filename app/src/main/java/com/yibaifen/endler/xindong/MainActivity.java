9f34f3f7a0d2fd5e855622b88ebc1c78679ad916
庞志伟 <zhiweip@oupeng.com>
2018-04-05 16:21:53 +0800

    over post to ms

--- app/src/main/java/com/yibaifen/endler/xindong/MainActivity.java
+++ app/src/main/java/com/yibaifen/endler/xindong/MainActivity.java
@@ -8,16 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import okhttp3.FormBody;
+
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import java.io.IOException;
 import android.util.Log;
+import com.google.gson.Gson;
+import com.yibaifen.endler.xindong.model.Face;
+import okhttp3.RequestBody;
 
 
 public class MainActivity extends AppCompatActivity {
     OkHttpClient client = new OkHttpClient();
-
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,18 +39,51 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                getRequest();
+                getRequest("https://maps.cc/girl/android_img.php",1);
             }
         });
 
 
     }
-    private void getRequest() {
+    private void xiaobing(String content){
+        Log.i("xiaobing",content);
+        postRequest(content,2);
+        //Gson gson = new Gson();
+        //Face user = gson.fromJson(content, Face.class);
+    }
+    private void callback(String content){
 
-        final Request request=new Request.Builder()
-                .get()
-                .tag(this)
-                .url("http://www.baidu.com")
+    }
+
+    private void postRequest(String json,final int stat) {
+
+        RequestBody formBody = null;
+        String url = null;
+        if(stat == 1) {
+            url = "http://kan.msxiaobing.com/Api/ImageAnalyze/Process?service=yanzhi&tid=e0bb5c47b3ce4994a645ba481a4ecf14";
+            Gson gson = new Gson();
+            Face user = gson.fromJson(json, Face.class);
+            Log.i("url",user.getHost() + user.getUrl());
+            formBody =
+                    new FormBody.Builder()
+                            .add("MsgId", String.valueOf(System.currentTimeMillis() * 1000))
+                            .add("CreateTime", String.valueOf(System.currentTimeMillis()))
+                            .add("Content[imageUrl]", user.getHost() + user.getUrl())
+                            .build();
+        }
+        if(stat == 2){
+            url = "http://kan.msxiaobing.com/Api/Image/UploadBase64";
+            formBody = RequestBody.create(JSON, json);
+        }
+        final Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .header("Cookie", "cpid=GDJbTCOyPEnZMClJfzFRSTU0ajFlTVpIXjPDTFo1VU1KAA; salt=BA587393E03E5A3CAAAE915769AFD20C; ARRAffinity=3b10bc35c1c993d8238753ddf09d68ede973452b125c86290c8b860ea0532de0; ai_user=Jgf7S|2018-04-05T03:53:29.343Z; ai_session=92GN0|1522900409451.2|1522900409451.2")
+                .header("Origin","http://kan.msxiaobing.com")
+                .header("User-Agent","Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36")
+                .header("Content-Type","application/x-www-form-urlencoded; charset=UTF-8")
+                .header("Referer","http://kan.msxiaobing.com/V3/Portal?task=yanzhi&ftid=3b769fdd8e184393a16df2cb931b5119")
+
                 .build();
 
         new Thread(new Runnable() {
@@ -52,7 +93,43 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     response = client.newCall(request).execute();
                     if (response.isSuccessful()) {
-                        Log.i("WY","打印GET响应的数据：" + response.body().string());
+                        String s =  response.body().string();
+                        if(stat == 2){
+                            postRequest(s,1);
+                        }
+                        //String re = new String(s.getBytes("UTF-8"), "UTF-8");
+                        Log.i("WY","打印POST响应的数据：" + s);
+
+                    } else {
+                        throw new IOException("Unexpected code " + response);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+    private void getRequest(String url, final int i) {
+
+        final Request request=new Request.Builder()
+                .get()
+                .tag(this)
+                .url(url)
+                .build();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response response = null;
+                try {
+                    response = client.newCall(request).execute();
+                    if (response.isSuccessful()) {
+                        String content= response.body().string();
+                        if(i == 1){
+                            xiaobing(content);
+                        }
+
                     } else {
                         throw new IOException("Unexpected code " + response);
                     }
---
+++ app/src/main/java/com/yibaifen/endler/xindong/model/Face.java
@@ -0,0 +1,29 @@
+package com.yibaifen.endler.xindong.model;
+
+/**
+ * Created by pangzhiwei on 2018/4/5.
+ */
+
+public class Face {
+
+    private String Host;
+
+    public String getHost() {
+        return Host;
+    }
+
+    public void setHost(String host) {
+        Host = host;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+
+    private String Url;
+
+}
